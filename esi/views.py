b5b16652001f41e66725fecc70c74a5143411ae1
AaronKable <aaronkable@gmail.com>
2020-10-05 18:38:45 +0800

    fix on mobile, and align beter, also remove duplicates.

--- esi/views.py
+++ esi/views.py
@@ -106,11 +106,27 @@ def select_token(request, scopes='', new=False):
 
     @tokens_required(scopes=scopes, new=new)
     def _token_list(r, tokens):
+        # Single Scope as string will break the Parser in the template
+        if isinstance(scopes, str):
+            scopes_output = [scopes]
+        else:
+            scopes_output = scopes
+        
+        # fake distint on character_name to not show duplicates
+        # MySQL doesn't support distint on field.
+        token_output = []
+        _characters = []
+        for t in tokens:
+            if t.character_name in _characters:
+                continue
+            token_output.append(t)
+            _characters.append(t.character_name)
 
         context = {
-            'tokens': tokens,
-            'scopes': scopes
+            'tokens': token_output,
+            'scopes': scopes_output
         }
+        
         return render(r, 'esi/select_token.html', context=context)
 
     return _token_list(request)
